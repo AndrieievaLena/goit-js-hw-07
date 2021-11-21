d9be5502a67b2b92a7d5a9929dd05a3a3fd0a849
AndrieievaLena <elenakovalevamod@gmail.com>
2021-11-21 20:10:40 +0200

    added gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryOfImages = document.querySelector('.gallery');
+const cardOfImage = onCreateGalleryItem(galleryItems);
+let openOriginalImage;
+
+galleryOfImages.insertAdjacentHTML('beforeend', cardOfImage);
+galleryOfImages.addEventListener('keydown', onPressEscToCloseImage);
+
+function onCreateGalleryItem(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+             <a class="gallery__link" href="large-image.jpg">
+                <img
+                    class="gallery__image lazyload"
+                    data-src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div> 
+        `
+    }).join('')
+}
+galleryOfImages.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+    openOriginalImage = basicLightbox.create(`
+		<img src="${e.target.dataset.source}" width="1280" height="900">
+	`)
+    openOriginalImage.show()
+});
+
+
+function onPressEscToCloseImage(event) {
+  if (event.code !== 'Escape') {
+      return;
+    }
+    openOriginalImage.close();
+}
